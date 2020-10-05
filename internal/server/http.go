1909a085a72635a17d8e0358096911cc779fb564
genndy007 <kochevgenazp@gmail.com>
2020-10-05 18:37:41 +0300

    Written basics of httpserver

---
+++ internal/server/http.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewHTTPServer(addr string) *http.Server {
+	httpsrv := newHTTPServer()
+	r := mux.NewRouter()
+	r.HandleFunc("/", httpsrv.handleProduce).Methods("POST")
+	r.HandleFunc("/", httpsrv.handleConsume).Methods("GET")
+
+	return &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+}
